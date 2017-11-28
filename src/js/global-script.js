dacb45feadf07f2d16edbefb1306f8b701bf6f60
Артем <amstelart@MacBook-Pro-Artem.local>
2017-11-28 15:02:29 +0300

    разметка

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,7 +1,78 @@
 // Если на проекте jQuery
-// $( document ).ready(function() {
-//   // code
-// });
+$( document ).ready(function() {
+  // code
+
+  $("#main-slider").owlCarousel({
+    items: 1,
+    nav: true,
+    loop: true,
+    center: true
+  });
+
+  $("#testimonies-carousel").owlCarousel({
+    items: 3,
+    nav: true,
+    loop: true,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 3,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 3,
+      },
+      1200 : {
+        items: 4,
+      },
+      1800 : {
+        items: 4,
+      }
+    }
+  });
+
+  $("#reviews-carousel").owlCarousel({
+    items: 3,
+    nav: true,
+    loop: true,
+    margin: 30,
+    center: false,
+    responsive : {
+      0 : {
+        items: 1,
+        nav: true,
+        loop: true,
+        center: true,
+      },
+      480 : {
+        items: 3,
+      },
+      768 : {
+        items: 3,
+      },
+      992 : {
+        items: 3,
+      },
+      1200 : {
+        items: 4,
+      },
+      1800 : {
+        items: 4,
+      }
+    }
+  });
+
+
+});
 
 // Изоляция без jQuery
 // (function(){
