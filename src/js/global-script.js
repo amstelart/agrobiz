f6df110b5bdfaeeb04e9954818c71b8ef8d0655a
Артем <amstelart@MacBook-Pro-Artem.local>
2017-11-29 16:57:20 +0300

    без адаптива

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -6,14 +6,17 @@ $( document ).ready(function() {
     items: 1,
     nav: true,
     loop: true,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     center: true
   });
 
   $("#testimonies-carousel").owlCarousel({
     items: 3,
     nav: true,
+    dots: false,
     loop: true,
     margin: 30,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     center: false,
     responsive : {
       0 : {
@@ -43,8 +46,10 @@ $( document ).ready(function() {
   $("#reviews-carousel").owlCarousel({
     items: 3,
     nav: true,
+    dots: false,
     loop: true,
     margin: 30,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
     center: false,
     responsive : {
       0 : {
